66b1ce596b9cb9809d639142e48106466c3e26c5
bonun00 <lifesciences00@naver.com>
2025-08-07 15:11:21 +0900

    msa root setting

---
+++ src/main/java/order/orderplatformmsa/OrderPlatformMsaApplication.java
@@ -0,0 +1,13 @@
+package order.orderplatformmsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderPlatformMsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderPlatformMsaApplication.class, args);
+	}
+
+}
